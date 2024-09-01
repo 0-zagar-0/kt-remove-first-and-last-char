4d502c2bc6c3a1eaff7c94af885446d485f13141
0-zagar-0 <git.hub.zagar@gmail.com>
2024-09-01 22:05:55 +0200

    implemented solution

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -3,6 +3,5 @@ package mate.academy
 const val REMOVAL_QUANTITY = 1
 
 fun removeChars(str: String): String {
-    // Implement this function
     return str.drop(REMOVAL_QUANTITY).dropLast(REMOVAL_QUANTITY)
 }
